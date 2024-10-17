bcb7bc727a1ccf80d32217710d0dc4246e50a5c3
Robert Farlow <robfarlow@gmail.com>
2024-10-17 10:57:34 -0700

    aws secrets manager

--- potato/lib/config.ts
+++ potato/lib/config.ts
@@ -1,10 +1,41 @@
 import dotenv from 'dotenv';
+import {
+	SecretsManagerClient,
+	GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager';
 
 dotenv.config({ path: '.env' });
 
-const Config = {
-	PORT: process.env.PORT || 25565,
+const client = new SecretsManagerClient({
+	region: 'us-west-1',
+});
+
+const response = await client.send(
+	new GetSecretValueCommand({
+		SecretId: 'prod/potato',
+		VersionStage: 'AWSCURRENT',
+	})
+);
+
+const secrets = JSON.parse(response.SecretString || '{}');
+
+interface ConfigType {
+	PORT: number;
+	REDIS_URL: string;
+}
+
+const DevConfig: ConfigType = {
+	PORT: Number(process.env.PORT) || 25565,
 	REDIS_URL: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
 };
 
+const ProdConfig: ConfigType = {
+	PORT: Number(process.env.PORT) || 25565,
+	REDIS_URL: secrets.REDIS_URL,
+};
+
+
+const Config = process.env.NODE_ENV === 'production' ? ProdConfig : DevConfig;
+
 export default Config;
+
--- potato/lib/redis.ts
+++ potato/lib/redis.ts
@@ -1,10 +1,22 @@
 import Redis from 'ioredis';
 import Config from './config';
 
-const redis = new Redis(Config.REDIS_URL);
+import getLogger from './logging';
+
+const logger = getLogger('redis');
+
+const redis = new Redis(Config.REDIS_URL, {
+	connectTimeout: 2000,
+	retryStrategy(times) {
+		if (times >= 3) {
+			return null;
+		}
+		return Math.min(times * 50, 2000);
+	},
+});
 
 redis.on('error', (error) => {
-	console.error(error);
+	logger.error('Redis error:', error);
 });
 
 export default redis;
